825da0c9d8dc2cbe37cd6f3a27a4bd2f667f4d5a
coolightman <drugak48@gmail.com>
2022-10-05 19:59:43 +0300

    update

--- app/src/main/java/by/coolightman/testapp/ui/MainActivity.kt
+++ app/src/main/java/by/coolightman/testapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package by.coolightman.testapp
+package by.coolightman.testapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import by.coolightman.testapp.ui.theme.TestAppTheme
 
 class MainActivity : ComponentActivity() {
