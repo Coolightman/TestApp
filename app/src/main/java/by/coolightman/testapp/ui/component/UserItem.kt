f6892ffea6a14d042ca32a88cd86490a612c475a
coolightman <drugak48@gmail.com>
2022-10-05 23:57:14 +0300

    update UI

--- app/src/main/java/by/coolightman/testapp/ui/component/UserItem.kt
+++ app/src/main/java/by/coolightman/testapp/ui/component/UserItem.kt
@@ -1,12 +1,12 @@
 package by.coolightman.testapp.ui.component
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -14,9 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import by.coolightman.testapp.R
 import by.coolightman.testapp.domain.model.User
@@ -32,7 +34,8 @@ fun UserItem(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() },
-        elevation = 2.dp
+        elevation = 2.dp,
+        shape = RoundedCornerShape(16.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -46,13 +49,34 @@ fun UserItem(
                 contentDescription = "user thumbnail",
                 placeholder = painterResource(id = R.drawable.ic_placeholder_24),
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.clip(CircleShape)
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(24.dp)
+                    .clip(CircleShape)
             )
-            Column {
-                Text(text = user.name)
-                Text(text = user.postsCount.toString())
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = user.name,
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = user.postsCount.toString(),
+                    style = MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
             }
-            Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp)
+                    .rotate(-90f)
+            )
         }
     }
 }
--- app/src/main/java/by/coolightman/testapp/ui/screens/userScreen/UserScreen.kt
+++ app/src/main/java/by/coolightman/testapp/ui/screens/userScreen/UserScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,6 +14,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import by.coolightman.testapp.R
@@ -35,22 +37,37 @@ fun UserScreen(
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
-                .clip(RoundedCornerShape(16.dp))
+                .fillMaxHeight(0.3f)
+                .clip(RoundedCornerShape(0.dp, 0.dp, 32.dp, 32.dp))
         )
         LazyColumn(
             state = listState,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(12.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 12.dp, vertical = 32.dp),
             modifier = Modifier.fillMaxSize()
         ) {
             items(items = uiState.posts, key = { it.id }) { post ->
                 Card(
                     modifier = Modifier.fillMaxWidth(),
-                    elevation = 2.dp
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(16.dp)
                 ) {
-                    Column(modifier = Modifier.fillMaxWidth()) {
-                        Text(text = post.title)
-                        Text(text = post.body)
+                    Column(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                    ) {
+                        Text(
+                            text = post.title,
+                            style = MaterialTheme.typography.h6,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = post.body,
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier.fillMaxWidth()
+                        )
                     }
                 }
             }
