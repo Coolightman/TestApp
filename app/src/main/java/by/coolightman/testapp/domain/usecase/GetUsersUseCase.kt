1224bbda1929c4967c72bb86dc8dfe511ebc372d
coolightman <drugak48@gmail.com>
2022-10-05 22:33:23 +0300

    update

--- app/src/main/java/by/coolightman/testapp/domain/usecase/GetUsersUseCase.kt
+++ app/src/main/java/by/coolightman/testapp/domain/usecase/GetUsersUseCase.kt
@@ -2,11 +2,23 @@ package by.coolightman.testapp.domain.usecase
 
 import by.coolightman.testapp.domain.repository.PostRepository
 import by.coolightman.testapp.domain.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetUsersUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val postRepository: PostRepository
 ) {
-    operator fun invoke() = userRepository.getUsers()
+    operator fun invoke() = flow {
+        emit(emptyList())
+        val list = userRepository.getUsers().first()
+        val postsCounts = list.map { it.id }.map { userId ->
+            postRepository.getPostsCountByUser(userId).first()
+        }
+        val users = list.zip(postsCounts) { user, count -> user.copy(postsCount = count) }
+        emit(users)
+    }.flowOn(Dispatchers.IO)
 }
