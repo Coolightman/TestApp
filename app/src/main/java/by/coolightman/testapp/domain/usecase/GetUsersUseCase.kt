19b95407612d0f4de275d379ee60a8e9dd7f5ac0
coolightman <drugak48@gmail.com>
2022-10-06 11:06:23 +0300

    update users use case

--- app/src/main/java/by/coolightman/testapp/domain/usecase/GetUsersUseCase.kt
+++ app/src/main/java/by/coolightman/testapp/domain/usecase/GetUsersUseCase.kt
@@ -1,29 +1,35 @@
 package by.coolightman.testapp.domain.usecase
 
+import by.coolightman.testapp.domain.model.User
 import by.coolightman.testapp.domain.repository.PostRepository
 import by.coolightman.testapp.domain.repository.UserRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.zip
 import javax.inject.Inject
 
 class GetUsersUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val postRepository: PostRepository
 ) {
-    operator fun invoke() = flow {
-        emit(emptyList())
-        val list = userRepository.getUsers().first()
-        val postsCounts = list.map { it.id }.pmap { userId ->
-            postRepository.getPostsCountByUser(userId).first()
+    suspend operator fun invoke(): Flow<List<User>> {
+        val list = userRepository.getUsers()
+        val postsCount = list.map {
+            it.pmap { user ->
+                postRepository.getPostsCountByUser(user.id).first()
+            }
         }
-        val users = list.zip(postsCounts) { user, count -> user.copy(postsCount = count) }
-        emit(users)
-    }.flowOn(Dispatchers.IO)
+        val users = list.zip(postsCount) { listUsers, listCounts ->
+            listUsers.zip(listCounts) { user, count ->
+                user.copy(postsCount = count)
+            }
+        }
+        return users
+    }
 }
 
 suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
